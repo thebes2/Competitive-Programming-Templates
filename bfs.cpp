342ad409edf738f06dd50d9d6ae78e5b2379edc9
George Chen <42050415+thebes2@users.noreply.github.com>
2018-08-04 00:49:23 -0400

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MN = 1e5+5;
+int N, M, i, x, y, vis[MN];
+vector<int> adj[MN];
+queue<int> q;
+
+int main(){
+  // finds all nodes that can be reached starting from node 1 and travelling along the given edges
+  for(scanf("%d%d",&N,&M),i=1;i<=M;i++){
+    scanf("%d%d",&x,&y);
+    adj[x].push_back(y);
+    adj[y].push_back(x);
+  }
+  q.push(1); vis[1]=1;
+  while(!q.empty()){
+    int cur = q.front(); q.pop();
+    for(auto v : adj[cur])
+      if(!vis[v]) vis[v]=1, q.push(v);
+  }
+  for(i=1;i<=N;i++)
+    printf("%d\n",vis[i]);
+   return 0;
+}
